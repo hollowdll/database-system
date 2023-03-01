4d4ac50a26e76ad59a22b0616ac4c2aaf5b3c4ac
hollowdll <pixerchat@gmail.com>
2023-03-01 21:03:00 +0200

    add initial structure for database manager

--- engine-core/src/lib.rs
+++ engine-core/src/lib.rs
@@ -1,4 +1,7 @@
 // Engine core library
+// Prototype phase
+// Will be organized and structured better later
+// This library will be split into modules later
 
 #![allow(unused)]
 
@@ -7,9 +10,31 @@ struct Config {}
 
 impl Config {
     pub fn build() -> Config {
-
-
         return Config {}
+    }
+}
+
+struct Database {
+
+}
+
+struct DatabaseTable {
+
+}
+
+struct DatabaseManager {
+    connected: bool,
+    database_count: u32,
+    databases: Vec<Database>,
+}
+
+impl DatabaseManager {
+    pub fn connect(&mut self) {
+        self.connected = true
+    }
+
+    pub fn disconnect(&mut self) {
+        self.connected = false
     }
 }
 
