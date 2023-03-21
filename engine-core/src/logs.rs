c76bce3cd915570e72ae6419e2df7495e90306c2
hollowdll <pixerchat@gmail.com>
2023-03-21 19:51:22 +0200

    add event log struct and improve logs

--- engine-core/src/logs.rs
+++ engine-core/src/logs.rs
@@ -10,7 +10,62 @@ use std::{
     fs,
     io::{self, Write},
     path::Path,
+    time::{
+        SystemTime,
+    },
 };
+
+#[derive(Debug)]
+enum DatabaseEventType {
+    Test,
+    Connected,
+}
+
+#[derive(Debug)]
+enum DatabaseEventLocation {
+    System,
+    DatabaseManager,
+    Database,
+    DatabaseTable,
+}
+
+struct DatabaseEventLog {
+    location: DatabaseEventLocation,
+    created: SystemTime,
+    event_type: DatabaseEventType,
+    log_text: String,
+}
+
+impl DatabaseEventLog {
+    fn format(&self) -> String {
+        let time = match self.created.duration_since(SystemTime::UNIX_EPOCH) {
+            Ok(time) => time.as_nanos(),
+            Err(e) => panic!("SystemTime error: {e}"),
+        };
+
+        format!("[{:?}] [{:?}] [{:?}] {}", self.location, time, self.event_type, self.log_text)
+    }
+}
+
+impl DatabaseEventLog {
+    fn from(location: DatabaseEventLocation, event_type: DatabaseEventType, log_text: &str) -> Self {
+        Self {
+            location,
+            created: SystemTime::now(),
+            event_type,
+            log_text: String::from(log_text),
+        }
+    }
+
+    fn create_test_log() -> Self {
+        Self {
+            location: DatabaseEventLocation::System,
+            created: SystemTime::now(),
+            event_type: DatabaseEventType::Test,
+            log_text: String::from("Test log 123"),
+        }
+    }
+}
 
 fn create_logs_dir() -> io::Result<()> {
     // Create logs directory to the project root
@@ -25,7 +80,7 @@ fn create_logs_dir() -> io::Result<()> {
     Ok(())
 }
 
-fn create_log_file(name: &str, content: &str) -> io::Result<()> {
+fn create_log_file(name: &str, content: String) -> io::Result<()> {
     if Path::new("./logs").is_dir() {
         let mut file = fs::File::create(format!("./logs/{name}"))?;
 
@@ -37,9 +92,15 @@ fn create_log_file(name: &str, content: &str) -> io::Result<()> {
     Ok(())
 }
 
+fn write_to_log_file() {
+    
+}
+
 pub fn create_test_log() -> Result<(), io::Error> {
     let log_name = "testlog.log";
-    let log_content = "[Date and time] [TEST] test log 123";
+    let log = DatabaseEventLog::create_test_log();
+    let log_content = log.format();
+    
 
     if let Err(e) = create_logs_dir() {
         eprintln!("Error: {e}");
