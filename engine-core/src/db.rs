abd8d827806fc401623ceb50d80e21c79cd43cc0
hollowdll <pixerchat@gmail.com>
2023-04-02 19:48:10 +0300

    'create database' creates initial data to json file

--- engine-core/src/db.rs
+++ engine-core/src/db.rs
@@ -7,31 +7,52 @@ use std::{
     path::Path, collections::HashMap,
 };
 
+/// Database structure for database files
 struct Database {
     name: String,
     collections: Vec<DatabaseDocumentCollection>,
 }
 
 impl Database {
-    fn new() {
-        
-    }
-
-    fn new_json() {
-
+    fn to_json(&self) -> serde_json::Value {
+        serde_json::json!({
+            "name": self.name,
+            "collections": [],
+        })
     }
 }
 
+impl Database {
+    fn from(name: &str) -> Self {
+        Self {
+            name: String::from(name),
+            collections: Vec::new(),
+        }
+    }
+
+    fn from_json(json: serde_json::Value) -> Self {
+        Self {
+            name: json["name"].to_string(),
+            collections: Vec::new(),
+        }
+    }
+}
+
+/// Database document collection
+/// that holds database documents.
 struct DatabaseDocumentCollection {
     name: String,
     documents: Vec<DatabaseDocument>,
 }
 
+/// Database document that holds
+/// data in key-value pairs
 struct DatabaseDocument {
     id: u64,
     data: HashMap<String, String>,
 }
 
+/// Creates databases directory in root directory
 pub fn create_databases_dir() -> io::Result<()> {
     if !Path::new("./databases").is_dir() {
         fs::create_dir("./databases")?;
@@ -40,12 +61,23 @@ pub fn create_databases_dir() -> io::Result<()> {
     Ok(())
 }
 
+/// Creates a database file in databases directory
+/// with initial data
 pub fn create_database_file(database_name: &str) -> io::Result<bool> {
-    if !Path::new(format!("./databases/{database_name}.json").as_str()).is_file() {
-        let mut file = fs::File::create(format!("./databases/{database_name}.json"))?;
+    let file_path = format!("./databases/{database_name}.json");
+
+    if !Path::new(&file_path).is_file() {
+        let mut file = fs::File::create(&file_path)?;
 
         // write initial data
+        let database = Database::from(database_name);
+        let json = database.to_json();
 
+        let mut file = OpenOptions::new()
+            .write(true)
+            .open(&file_path)?;
+
+        file.write(serde_json::to_string_pretty(&json)?.as_bytes())?;
 
     } else {
         return Ok(false);
@@ -54,6 +86,7 @@ pub fn create_database_file(database_name: &str) -> io::Result<bool> {
     Ok(true)
 }
 
+/// Check if a database file exists in databases directory
 pub fn database_file_exists(database_name: &str) -> bool {
     if Path::new(format!("./databases/{database_name}.json").as_str()).is_file() {
         return true;
