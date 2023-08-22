3f1566a3836a2beede8aed44ceabe37356e9a477
hollowdll <pixerchat@gmail.com>
2023-08-22 19:38:39 +0300

    add test 'create_database_by_file_path_success'

--- engine/tests/database.rs
+++ engine/tests/database.rs
@@ -6,6 +6,7 @@ use engine::{
     config::Config,
     Logger,
     Engine,
+    storage::DB_FILE_EXTENSION,
 };
 use tempfile::tempdir;
 
@@ -21,13 +22,17 @@ fn create_database_to_db_dir_success() {
     let engine = Engine::build(&config, &logger);
     let db_name = "test";
 
-    let result = engine.storage_api().create_database_to_db_dir(db_name);
+    let result = engine
+        .storage_api()
+        .create_database_to_db_dir(db_name);
     assert!(result.success);
     assert!(result.data.is_none());
     assert!(result.error.is_none());
     assert!(result.log_error.is_none());
 
-    let result = engine.storage_api().find_database(db_name);
+    let result = engine
+        .storage_api()
+        .find_database(db_name);
     assert!(result.success);
     assert!(result.data.is_some());
     assert!(result.error.is_none());
@@ -41,3 +46,41 @@ fn create_database_to_db_dir_success() {
     logs_dir.close().unwrap();
 }
 
+#[test]
+fn create_database_by_file_path_success() {
+    let db_dir = tempdir().unwrap();
+    let logs_dir = tempdir().unwrap();
+    let config = Config::new(
+        db_dir.path(),
+        logs_dir.path()
+    );
+    let logger = Logger::build(&config);
+    let engine = Engine::build(&config, &logger);
+    let db_name = "test";
+    let file_path = db_dir
+        .path()
+        .join(&format!("{}.{}", db_name, DB_FILE_EXTENSION));
+
+    let result = engine
+        .storage_api()
+        .create_database_by_file_path(db_name, &file_path);
+    assert!(result.success);
+    assert!(result.data.is_none());
+    assert!(result.error.is_none());
+    assert!(result.log_error.is_none());
+
+    let result = engine
+        .storage_api()
+        .find_database_by_file_path(&file_path);
+    assert!(result.success);
+    assert!(result.data.is_some());
+    assert!(result.error.is_none());
+    assert!(result.log_error.is_none());
+
+    let db = result.data.unwrap();
+    assert!(db.is_some());
+    assert_eq!(db.unwrap().name(), db_name);
+
+    db_dir.close().unwrap();
+    logs_dir.close().unwrap();
+}
