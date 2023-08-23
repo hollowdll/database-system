3cb25960a1e4e5e95f4a36404e52ed4bf21ca457
hollowdll <pixerchat@gmail.com>
2023-08-23 20:25:50 +0300

    add test 'find_database_success'

--- engine/tests/database.rs
+++ engine/tests/database.rs
@@ -199,3 +199,39 @@ fn find_all_databases_success() {
     db_dir.close().unwrap();
     logs_dir.close().unwrap();
 }
+
+#[test]
+fn find_database_success() {
+    let db_dir = tempdir().unwrap();
+    let logs_dir = tempdir().unwrap();
+    let config = Config::new(
+        db_dir.path(),
+        logs_dir.path()
+    );
+    let logger = Logger::build(&config);
+    let engine = Engine::build(&config, &logger);
+    let db_name = "test";
+    let file_path = db_dir
+        .path()
+        .join(&format!("{}.{}", db_name, DB_FILE_EXTENSION));
+
+    let result = engine
+        .storage_api()
+        .create_database_by_file_path(db_name, &file_path);
+    assert!(result.success);
+
+    let result = engine
+        .storage_api()
+        .find_database(db_name);
+    assert!(result.success);
+    assert!(result.data.is_some());
+    assert!(result.error.is_none());
+    assert!(result.log_error.is_none());
+
+    let db = result.data.unwrap();
+    assert!(db.is_some());
+    assert_eq!(db.unwrap().name(), db_name);
+
+    db_dir.close().unwrap();
+    logs_dir.close().unwrap();
+}
