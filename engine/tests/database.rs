26d5cc0c505ca8736ca7c71c6c55b9f56c0736ab
hollowdll <pixerchat@gmail.com>
2023-08-23 20:24:37 +0300

    add test 'find_all_databases_success'

--- engine/tests/database.rs
+++ engine/tests/database.rs
@@ -26,7 +26,6 @@ fn create_database_to_db_dir_success() {
     let result = engine
         .storage_api()
         .create_database_to_db_dir(db_name);
-
     assert!(result.success);
     assert!(result.data.is_none());
     assert!(result.error.is_none());
@@ -35,7 +34,6 @@ fn create_database_to_db_dir_success() {
     let result = engine
         .storage_api()
         .find_database(db_name);
-
     assert!(result.success);
     assert!(result.data.is_some());
     assert!(result.error.is_none());
@@ -67,7 +65,6 @@ fn create_database_by_file_path_success() {
     let result = engine
         .storage_api()
         .create_database_by_file_path(db_name, &file_path);
-
     assert!(result.success);
     assert!(result.data.is_none());
     assert!(result.error.is_none());
@@ -76,7 +73,6 @@ fn create_database_by_file_path_success() {
     let result = engine
         .storage_api()
         .find_database_by_file_path(&file_path);
-    
     assert!(result.success);
     assert!(result.data.is_some());
     assert!(result.error.is_none());
@@ -108,20 +104,17 @@ fn delete_database_success() {
     let result = engine
         .storage_api()
         .create_database_by_file_path(db_name, &file_path);
-
     assert!(result.success);
 
     let result = engine
         .storage_api()
         .find_database_by_file_path(&file_path);
-
     assert!(result.success);
     assert!(result.data.unwrap().is_some());
 
     let result = engine
         .storage_api()
         .delete_database(&file_path);
-
     assert!(result.success);
     assert!(result.data.is_none());
     assert!(result.error.is_none());
@@ -130,7 +123,6 @@ fn delete_database_success() {
     let result = engine
         .storage_api()
         .find_database_by_file_path(&file_path);
-
     assert!(result.success);
     assert!(result.data.unwrap().is_none());
 
@@ -157,17 +149,52 @@ fn change_database_description_success() {
     let result = engine
         .storage_api()
         .create_database_by_file_path(db_name, &file_path);
-    
     assert!(result.success);
 
     let result = engine
         .storage_api()
         .change_database_description(&file_path, description);
-
     assert!(result.success);
     assert!(result.data.is_none());
     assert!(result.error.is_none());
     assert!(result.log_error.is_none());
+
+    db_dir.close().unwrap();
+    logs_dir.close().unwrap();
+}
+
+#[test]
+fn find_all_databases_success() {
+    let db_dir = tempdir().unwrap();
+    let logs_dir = tempdir().unwrap();
+    let config = Config::new(
+        db_dir.path(),
+        logs_dir.path()
+    );
+    let logger = Logger::build(&config);
+    let engine = Engine::build(&config, &logger);
+    let db_name = "test";
+    let file_path = db_dir
+        .path()
+        .join(&format!("{}.{}", db_name, DB_FILE_EXTENSION));
+
+    let result = engine
+        .storage_api()
+        .create_database_by_file_path(db_name, &file_path);
+    assert!(result.success);
+
+    let result = engine
+        .storage_api()
+        .find_all_databases();
+    assert!(result.success);
+    assert!(result.data.is_some());
+    assert!(result.error.is_none());
+    assert!(result.log_error.is_none());
+
+    let databases = result.data.unwrap();
+    let first_db = databases.get(0).unwrap();
+    assert_eq!(databases.len(), 1);
+    assert_eq!(first_db.name(), db_name);
 
     db_dir.close().unwrap();
     logs_dir.close().unwrap();
