65367d3af97bc022c173683083bbccbe027cb9d3
hollowdll <pixerchat@gmail.com>
2023-08-26 20:25:58 +0300

    add test 'find_collection_success'

--- engine/tests/collection/find_collection.rs
+++ engine/tests/collection/find_collection.rs
@@ -30,9 +30,46 @@ fn find_all_collections_success() {
         .storage_api()
         .find_all_collections(&file_path);
     assert!(result.success);
+    assert!(result.error.is_none());
+    assert!(result.log_error.is_none());
 
     let collections = result.data.unwrap();
     assert_eq!(collections.len(), 1);
+
+    config_settings.db_dir.close().unwrap();
+    config_settings.logs_dir.close().unwrap();
+}
+
+#[test]
+fn find_collection_success() {
+    let config_settings = ConfigSettings::new();
+    let logger = Logger::build(&config_settings.config);
+    let engine = Engine::build(&config_settings.config, &logger);
+    let db_name = "test";
+    let collection_name = "people";
+    let file_path = config_settings.db_dir
+        .path()
+        .join(&format!("{}.{}", db_name, DB_FILE_EXTENSION));
+
+    let result = engine
+        .storage_api()
+        .create_database_by_file_path(db_name, &file_path);
+    assert!(result.success);
+
+    let result = engine
+        .storage_api()
+        .create_collection(collection_name, &file_path);
+    assert!(result.success);
+
+    let result = engine
+        .storage_api()
+        .find_collection(collection_name, &file_path);
+    assert!(result.success);
+    assert!(result.error.is_none());
+    assert!(result.log_error.is_none());
+
+    let collection = result.data.unwrap();
+    assert_eq!(collection.unwrap().name(), collection_name);
 
     config_settings.db_dir.close().unwrap();
     config_settings.logs_dir.close().unwrap();
