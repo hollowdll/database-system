717251dd960b8a6cff0c238d066e2423fe7f2d20
hollowdll <pixerchat@gmail.com>
2023-09-03 17:28:59 +0300

    add test 'set_db_dir_path_and_load_config'

--- engine/tests/config/logs_dir.rs
+++ engine/tests/config/logs_dir.rs
@@ -11,22 +11,22 @@ fn set_logs_dir_path_success() {
     let config_settings = ConfigSettings::new();
     let logger = Logger::build(&config_settings.config);
     let engine = Engine::build(&config_settings.config, &logger);
-    let dir = tempdir().unwrap();
+    let new_dir = tempdir().unwrap();
     let config_file = fs::File::create(config_settings.config.config_file_path()).unwrap();
 
     let result = engine
         .config_api()
-        .set_logs_dir_path(dir.path());
+        .set_logs_dir_path(new_dir.path());
     assert!(result.success);
     assert!(result.error.is_none());
     assert!(result.log_error.is_none());
 
     let content = fs::read_to_string(config_settings.config.config_file_path()).unwrap();
-    assert!(content.contains(&format!("{:?}", dir.path())));
+    assert!(content.contains(&format!("{:?}", new_dir.path())));
 
     drop(config_file);
     config_settings.config_dir.close().unwrap();
     config_settings.db_dir.close().unwrap();
     config_settings.logs_dir.close().unwrap();
-    dir.close().unwrap();
+    new_dir.close().unwrap();
 }
