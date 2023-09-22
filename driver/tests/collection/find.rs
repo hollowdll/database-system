2e3819ed223630d991eec93f66715b6cc5db9c48
hollowdll <pixerchat@gmail.com>
2023-09-22 20:59:57 +0300

    driver find document by id test

--- driver/tests/collection/find.rs
+++ driver/tests/collection/find.rs
@@ -32,3 +32,31 @@ pub fn find_all_documents_success() {
 
     config.close_temp_dirs();
 }
+
+#[test]
+pub fn find_one_document_by_id_success() {
+    let config = Config::new();
+    let client = DatabaseClient::build(config.db_dir.path());
+    let db_name = "testdb123";
+    let collection_name = "collection1";
+    let file_path = config.db_dir
+        .path()
+        .join(&format!("{}.{}", db_name, DB_FILE_EXTENSION));
+    let database = client.get_database(db_name).unwrap();
+
+    assert_eq!(database.connection_string(), &file_path);
+    assert!(file_path.is_file());
+
+    let collection = database.get_collection(collection_name).unwrap();
+    assert_eq!(collection.name(), collection_name);
+
+    let document = create_test_document();
+    let created_document = collection.insert_one(document).unwrap();
+    let found_document = collection.find_one_by_id(created_document.id()).unwrap();
+
+    let found_document = found_document.unwrap();
+    assert_eq!(found_document.id.0, created_document.id.0);
+    assert_eq!(found_document.data.len(), created_document.data.len());
+
+    config.close_temp_dirs();
+}
