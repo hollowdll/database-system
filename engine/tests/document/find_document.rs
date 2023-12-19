43636521b9a9d5be3620320ba43d5a580118578e
hollowdll <pixerchat@gmail.com>
2023-12-19 19:15:17 +0200

    find documents with query api test

--- engine/tests/document/find_document.rs
+++ engine/tests/document/find_document.rs
@@ -1,6 +1,7 @@
 use engine::{
     Engine,
     storage::DB_FILE_EXTENSION,
+    DocumentInputDataField,
 };
 use crate::common::{
     ConfigSettings,
@@ -88,6 +89,53 @@ fn find_document_by_id_success() {
     let document = result.data.unwrap().unwrap();
     assert_eq!(document.id(), created_document.id());
     assert_eq!(document.data().len(), created_document.data().len());
+
+    config_settings.close_temp_dirs();
+}
+
+#[test]
+fn find_documents_success() {
+    let config_settings = ConfigSettings::new();
+    let engine = Engine::build(&config_settings.config);
+    let db_name = "test";
+    let collection_name = "people";
+    let file_path = config_settings.db_dir
+        .path()
+        .join(&format!("{}.{}", db_name, DB_FILE_EXTENSION));
+    let max_documents = 3;
+    let expected_document_count = max_documents - 1;
+
+    engine.storage_api()
+        .create_database_by_file_path(db_name, &file_path);
+    
+    engine.storage_api()
+        .create_collection(collection_name, &file_path);
+
+    for i in 1..=max_documents {
+        let mut data = Vec::new();
+        if i == 1 {
+            data.push(DocumentInputDataField::new("age", "Int32", "30"));
+        } else {
+            data.push(DocumentInputDataField::new("age", "Int32", "35"));
+        }
+
+        engine.storage_api()
+            .create_document(&file_path, collection_name, data);
+    }
+
+    let mut query = Vec::new();
+    query.push(DocumentInputDataField::new("age", "Int32", "35"));
+    let result = engine
+        .storage_api()
+        .find_documents(&file_path, collection_name, query);
+    assert!(result.success);
+    assert!(result.data.is_some());
+    assert!(result.error.is_none());
+    assert!(result.log_error.is_none());
+
+    let documents = result.data.unwrap();
+    assert!(documents.len() > 0);
+    assert_eq!(documents.len(), expected_document_count);
 
     config_settings.close_temp_dirs();
 }
