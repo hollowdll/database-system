1d60edccae9f00c0914c14ea10a725d4230dcd57
hollowdll <pixerchat@gmail.com>
2023-12-22 19:57:50 +0200

    util tests

--- engine/src/util.rs
+++ engine/src/util.rs
@@ -9,3 +9,17 @@ pub fn has_whitespaces(input: &str) -> bool {
     }
     false
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::util::has_whitespaces;
+
+    #[test]
+    fn test_has_whitespaces() {
+        let input1 = "no";
+        assert!(!has_whitespaces(input1));
+
+        let input2 = "ye s";
+        assert!(has_whitespaces(input2));
+    }
+}
